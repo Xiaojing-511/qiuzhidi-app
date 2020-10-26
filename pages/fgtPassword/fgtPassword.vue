daee7b99f7b1d35a84dc843a52b7e2814bd8a94d
wq <847176563qq.com>
2020-10-26 21:59:58 +0800

    找回密码样式重置

--- pages/fgtPassword/fgtPassword.vue
+++ pages/fgtPassword/fgtPassword.vue
@@ -1,24 +1,50 @@
 <template>
 	<view class="content">
 		<view class="fgt-group">
-			<view class="u-border-bottom">
+			<view class="top_text">
+				<text>使用注册的手机号找回密码</text>
+			</view>
+			<view class="box">
+				<view class="img_box">
+					<image class="fgt_img" src="../../static/img/phone.png" mode=""></image>
+				</view>
 				<input class="fgt-input u-m-t-50 u-p-b-20" type="number" v-model="phone" placeholder="请输入手机号" :placeholder-style="input_style" />
+			</view>
+			<!-- 	<view class="getCode_btn">
 				<u-button v-show="isShow" @click="getCode" :custom-style="customStyle_GetCode">获取验证码</u-button>
 				<u-button v-show="!isShow" disabled="true" :custom-style="customStyle_GetCode">还有{{timer}}秒</u-button>
-			</view>
-			
-			<!-- <u-button v-show="isShow" @click="getCode" :custom-style="customStyle_GetCode">获取验证码</u-button>
-			<u-button v-show="!isShow" disabled="true" :custom-style="customStyle_GetCode">还有{{timer}}秒</u-button> -->
-			<view class="u-border-bottom">
+			</view> -->
+			<view class="box" >
+				<view class="img_box">
+					<image class="fgt_img" src="../../static/img/3.png" mode=""></image>
+				</view>
 				<input class="fgt-input u-m-t-50 u-p-b-20" type="number" v-model="code" placeholder="请输入验证码" :placeholder-style="input_style" />
+				<view class="getCode_btn">
+<!-- 					\<button class="getCode" v-show="isShow" @click="getCode" >获取验证码</button>
+					<button class="getCode" v-show="!isShow" disabled="true" >还有{{timer}}秒</button> -->
+					<a class="getCode" v-show="isShow" @click="getCode" >获取验证码</a>
+					<a class="getCode" v-show="!isShow" disabled="true" >还有{{timer}}秒</a>
+				</view>
 			</view>
-			<view class="u-border-bottom">
-				<input class="fgt-input u-m-t-50 u-p-b-20" type="password" v-model="password" placeholder="请输入新密码":placeholder-style="input_style" />
+			<view class="box">
+				<view class="img_box">
+					<image class="fgt_img" src="../../static/img/9.png" mode=""></image>
+				</view>
+				
+				<input class="fgt-input u-m-t-50 u-p-b-20" type="password" v-model="password" placeholder="请输入新密码"
+				 :placeholder-style="input_style" />
+
 			</view>
-			<view class="u-border-bottom">
-				<input class="fgt-input u-m-t-50 u-p-b-20" type="password" v-model="repassword" placeholder="请确认新密码":placeholder-style="input_style" />
+			<view class="box">
+				<view class="img_box">
+					<image class="fgt_img" src="../../static/img/9.png" mode=""></image>
+				</view>
+				
+				<input class="fgt-input u-m-t-50 u-p-b-20" type="password" v-model="repassword" placeholder="请确认新密码"
+				 :placeholder-style="input_style" />
+
 			</view>
-			<u-button @click="changeps" :custom-style="customStyle_Push" shape="circle">注册</u-button>
+			<u-button @click="changeps" :custom-style="customStyle_Push">注册</u-button>
 		</view>
 		<u-toast ref="uToast" />
 	</view>
@@ -31,26 +57,31 @@
 				phone: '',
 				timer: 60,
 				isShow: true,
-				code:'',
-				input_style: 'color:#FFFFFF;font-size:30rpx;',
-				customStyle_GetCode:{
-					width: '200rpx',
-					height: '80rpx',
-					color: '#FFFFFF',
-					fontSize: '30rpx',
-					background: '#00A8FF',
-					marginTop:'32rpx',
-					marginBottom:'20rpx'
+				code: '',
+				input_style: 'color:#999999;font-size:30rpx;',
+				// customStyle_GetCode: {
+				// 	width: '100rpx',
+				// 	height: '110rpx',
+				// 	color: '#00A8FF',
+				// 	fontSize: '30rpx',
+				// 	background: '#FFFFFF',
+				// 	marginTop: '50rpx',
+				// 	marginBottom: '20rpx',
 					
-				},
+					
+					
 				
+					
+
+				// },
+
 				customStyle_Push: {
-					width: '200rpx',
-					height: '80rpx',
-					margin: '60rpx auto',
-					color: '#00A8FF',
+					width: '600rpx',
+					height: '90rpx',
+					margin: '100rpx auto',
+					color: '#ffffff',
 					fontSize: '36rpx',
-					background: '#ffffff'
+					background: '#00A8FF'
 				}
 			}
 		},
@@ -97,15 +128,15 @@
 				//此部分为验证码是否与短信的匹配
 				// }
 				else {
-					this.$u.post('/sso/updatePassword' , {
+					this.$u.post('/sso/updatePassword', {
 						telephone: this.phone,
 						password: this.password,
-						authCode:this.code
+						authCode: this.code
 
 					}).then((res) => {
 						console.log(res)
 						uni.navigateTo({
-							url:'../login/login'
+							url: '../login/login'
 						})
 					}).catch(res => {
 						console.log(res)
@@ -126,22 +157,91 @@
 
 <style lang="scss" scoped>
 	.content {
-		background: $color_emphasize;
+		background-color: #EEEEEE;
 		width: 100%;
+		height: 100%;
+		position: absolute;
+	}
+
+	.fgt-input {
+		padding-left:20rpx ;
+		padding-top: 2rpx;
+	}
+	
+	.getCode_btn{
+		position: absolute;
+		right: 100rpx;
+		top: 340rpx;
+		border-left:1rpx solid rgba($color: #00A8FF, $alpha: 1.0) ;
+		height: 70rpx;
+		
+	
+	}
+	.getCode_btn a {
+		height: 100%;
+		line-height: 100%;
+		width: 100%;
+		margin-left: 20rpx;
+		margin-top:20rpx ;
+		
+	}
+	
+	
+	.img_box{
+		background-color: #FFFFFF;
+		width: 100rpx;
+		height: 110rpx;
+		margin-top:50rpx ;
+		border-right:1rpx solid rgba($color: #000000, $alpha: 0.5) ;
+	
+	}
+
+	.top_text text {
+
+		color: #999999;
+		font-size: 24rpx;
+	}
+
+	.fgt_img {
+		width: 40rpx;
+		height: 64rpx;
+		background-color: #FFFFFF;
+		display: block;
+		margin: 20rpx auto;
+		line-height: 64rpx;
+		
 	}
 
 	.fgt-group {
 		display: block;
-		width: 500rpx;
-		margin: 250rpx auto;
+		width: 600rpx;
+		margin: 80rpx auto;
+		
 	}
 	
-	.u-border-bottom{
+	.getCode {
+		
+			width: 100rpx;
+			height: 110rpx;
+			color: #00A8FF;
+			fontSize: 30rpx;
+			background: #FFFFFF;
+			margin: 56rpx 0 20rpx 0;
+			
+	}
+	
+	
+	
+
+	.box {
 		display: flex;
+		
 	}
-	
+
 	.fgt-input {
-		color: #FFFFFF;
-		font-size: $font_general_big;
+		width: 600rpx;
+		height: 90rpx;
+		background: #FFFFFF;
+		border-radius: 5rpx;
 	}
 </style>
