bf3c93ae8f61f70f2615d9b96c0266dfd96a76d4
wq <847176563qq.com>
2020-10-29 16:00:42 +0800

    面试官编辑页基本完成 差优化

--- pages/interviewer_editor/interviewer_editor.vue
+++ pages/interviewer_editor/interviewer_editor.vue
@@ -2,73 +2,147 @@
 	<view class="content">
 		<view class="iner_box">
 
+			<view class="head_img">
+				<u-upload ref="uUpload"  :max-count='1' width='120rpx' height="120rpx" 
+				 :file-list="fileList" :before-upload="beforeUpload"></u-upload>
+			</view>
 			<view class="first_box">
 				<view class="tip_text">
 					<text>基本信息</text>
 				</view>
 				<view class="msg">
-					
 					<view class="tip">
-						昵称:<input type="text" v-model="name" value="" />
+						昵称:<input class="" type="text" v-model="nickname" value="" />
 					</view>
-					
 					<view class="tip">
-						职位:<input type="text" v-model="place" value="" />
+						职位:<input type="text" v-model="job" value="" />
 					</view>
-					
 					<view class="tip">
-						工龄:<input type="text" v-model="age" value="" />
+						工龄:<input type="text" v-model="gender" value="" />
 					</view>
-					
+
 					<view class="tip">
-						公司:<input type="text" v-model="company" value="" />
+						公司:<text>{{companyName}}</text>
 					</view>
-					
-					<view class="tip_img">
-						评分:<image class="img_star" src="" mode=""></image>
-						<image class="img_star" src=""  mode=""></image>
+					<view class="tip">
+						评分：<u-rate active-color="#FA3534" :current="4" :disabled="true" inactive-color="#b2b2b2" gutter="20"></u-rate>
 					</view>
-					
-					
 				</view>
 			</view>
-
 			<view class="second_box">
 				<view class="tip_text">
 					<text>工作经历</text>
-					<input type="text" v-model="story" value="" />
+
+					<textarea class="text-area" v-model='workExperience' cols="" rows="" style="vertical-align:top;outline:none;"></textarea>
 				</view>
-
-
-
 			</view>
-
 			<view class="third_box">
 				<view class="tip_text">
 					<text>面试侧重点</text>
-					<view class="tip_text_iner">
-						
-					</view>
+
+					<textarea class="text-area" v-model="focus" cols="" rows="" style="vertical-align:top;outline:none;"></textarea>
+				</view>
+
+				<view class="btn_box">
+					<button class="btn" @click="postInfo">提交</button>
 				</view>
 			</view>
-
-
 		</view>
+
 	</view>
 </template>
-
 <script>
 	export default {
 		data() {
 			return {
-				name:"",
-				place:"",
-				age:"",
-				company:"",
-				story:''
+				nickname: "",
+				icon: '',
+				companyName: '',
+				gender: 0,
+				city: '',
+				job: '',
+				personalizedSignature: '',
+				workExperience: '',
+				focus: '',
+				fileList: [],
+			
+
 			}
 		},
+		onLoad() {
+			this.getInfo()
+			console.log(this)
+		},
+
 		methods: {
+
+			beforeUpload(index, list) {
+				// 只上传偶数索引的文件
+				console.log(list[0].url)
+				let that = this
+				uni.uploadFile({
+					url: 'http://118.24.96.51:8085/file/single', // 后端api接口
+					filePath: list[0].url, // uni.chooseImage函数调用后获取的本地文件路劲
+					sizeType: 'compressed',
+					formData: {
+						dir: 'static/img'
+					},
+					name: 'file', //后端通过'file'获取上传的文件对象
+					header: {
+						Authorization: `Bearer` + that.vuex_token
+					},
+					success: (res) => {						
+						that.icon = JSON.parse(res.data).data
+						console.log(that.icon)
+						
+					}
+				})
+			},
+			getInfo() {
+				this.$u.post('/interviewer/details', {
+
+				}).then((res) => {
+					console.log(res);
+					this.nickname = res.nickname;
+					this.icon = res.icon;
+					this.gender = res.gender;
+					this.city = res.city;
+					this.job = res.job;
+					this.personalizedSignature = res.personalizedSignature;
+					this.workExperience = res.workExperience;
+					this.focus = res.focus;
+					this.companyName = res.companyName;
+					let a = []
+					a.push({
+						url: 'http://118.24.96.51:8085/' + res.icon
+					})
+					this.fileList = a
+
+				})
+
+
+
+			},
+
+
+			postInfo() {
+				this.$u.post('/interviewer/submit/changes', {
+					nickname: this.nickname,
+					icon: this.icon,
+					gender: this.gender,
+					city: this.city,
+					job: this.job,
+					personalizedSignature: this.personalizedSignature,
+					workExperience: this.workExperience,
+					focus: this.focus,
+
+				}).then((res) => {
+					
+
+				})
+			},
+
+
 
 		}
 	}
@@ -78,6 +152,7 @@
 	.contect {
 		height: 100%;
 		width: 100%;
+		position: absolute;
 	}
 
 	.iner_box {
@@ -99,19 +174,39 @@
 	.msg {
 		margin-top: 60rpx;
 		margin-left: 60rpx;
-		
-}
+
+	}
+
 	.msg text {
 		display: flex;
 	}
-	
-	.tip{
+
+	.tip {
 		display: flex;
 		margin-bottom: 20rpx;
-		
+
 		font-size: 30rpx;
 		font-family: PingFang;
 		font-weight: 500;
 		color: #333333;
+	}
+
+	.text-area {
+		margin-top: 20rpx;
+
+		font-size: 30rpx;
+		font-family: PingFang;
+		font-weight: 500;
+		color: #999999;
+	}
+
+	.head_img {
+		position: absolute;
+		right: 80rpx;
+		top: 180rpx;
+	}
+
+	.upld {
+		display: flex;
 	}
 </style>
